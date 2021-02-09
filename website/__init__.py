46919b39f07ec4c13e47f69469cdae1270f57e76
Mikolaj Noga <nogamikolaj00@gmail.com>
2021-02-09 20:50:01 +0100

    Create routes and views

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'OFq*88f2HB +#!Mfaa8{<BD'
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
